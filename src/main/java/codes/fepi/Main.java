ace89a8dde5dd7e02cc694233d03caccae37037a
AUTplayed <fips.hem@gmail.com>
2020-06-17 23:07:44 +0200

    fixed fatJar

--- src/main/java/codes/fepi/Main.java
+++ src/main/java/codes/fepi/Main.java
@@ -3,7 +3,6 @@ package codes.fepi;
 import codes.fepi.data.Repository;
 import codes.fepi.entities.Item;
 import codes.fepi.ldfspark.LdfSpark;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import spark.Spark;
 
@@ -14,11 +13,11 @@ import java.util.List;
 public class Main {
 
 	private static final ObjectMapper MAPPER = new ObjectMapper();
-	private static final TypeReference ref = new TypeReference<List<Item>>() {
-	};
 
 	public static void main(String[] args) throws URISyntaxException, IOException {
-		Repository.INSTANCE.initData(MAPPER.readValue(Main.class.getClassLoader().getResource("data.json"), ref));
+
+		Repository.INSTANCE.initData(MAPPER.readValue(Main.class.getClassLoader().getResource("data.json"),
+				MAPPER.getTypeFactory().constructCollectionType(List.class, Item.class)));
 		Spark.port(6020);
 		LdfSpark.start();
 	}
